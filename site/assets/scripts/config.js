b75030b026a75cab33bf84af27af431ca8ed385e
Mike Nosal <mnosal@mitre.org>
2016-07-12 09:36:44 -0400

    Initial placeholder website for Synthetic Mass and the data visualization dashboard

---
+++ site/assets/scripts/config.js
@@ -0,0 +1,39 @@
+/* Styles for map features */
+module.exports = {
+  baseStyle : {
+    color:'black',
+    fillColor:'none',
+    weight:5
+  },
+  copyFeature :{  
+    weight:4,
+    fillColor:'black',
+    fillOpacity:0.5,
+    opacity:1,
+    color:'red'
+  },
+  fillOpacity: 0.66,
+  borderColor : "#fff",
+  borderOpacity: 0.6,
+  newFeature :{  
+    weight:3,
+    fillColor:'none',
+    fillOpacity:0,
+    opacity:1,
+    color:'#444'
+  },
+  overlays : {
+    color:"#f60",
+    fillColor:'#222',
+    fillOpacity:0.4,
+    opacity:0.7,
+    weight:2
+  },
+  selectedFeature :  {  
+    weight:4,
+    fillColor:'none',
+    fillOpacity:0,
+    opacity:1,
+    color:'black'
+  }
+}
