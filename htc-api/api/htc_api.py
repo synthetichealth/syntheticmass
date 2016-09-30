76ed36ffd99ed41d369140115287fd1681207e89
root <root@syntheticmass-dev.mitre.org>
2016-09-29 21:30:24 -0400

    added diabetes at town level based on predictions (census)

--- htc-api/api/htc_api.py
+++ htc-api/api/htc_api.py
@@ -468,9 +468,10 @@ def get_cousub_all():
     sql = "SELECT s.ct_fips, s.ct_name, s.cs_fips, s.cs_name, s.sq_mi, s.pop, s.pop_sm, " \
         "CASE WHEN s.pop > 0 THEN s.pop_male / s.pop ELSE 0 END AS pct_male, " \
         "CASE WHEN s.pop > 0 THEN s.pop_female / s.pop ELSE 0 END AS pct_female, " \
-	"op.deaths AS opioid_deaths, " \
+	"op.deaths AS opioid_deaths, pred.pred_diabetes as pct_diabetes, " \
         "ST_AsGeoJSON(g.the_geom) AS geometry " \
     	"FROM synth_ma.cousub_stats s " \
+	"JOIN synth_ma.ma_cousub_pred pred ON pred.state_fips = '25' AND pred.county = s.ct_fips AND pred.county_sub_fips = s.cs_fips AND s.cs_fips != '00000' " \
 	"JOIN tiger_cb14_500k.cousub g ON g.statefp = '25' AND g.countyfp = s.ct_fips AND g.cousubfp = s.cs_fips AND s.cs_fips != '00000' " \
 	"JOIN synth_ma.ma_opioid2 op ON op.cousubfp = s.cs_fips AND s.cs_fips != '00000' AND year = '2015'"
     data = p2g.getData(con, sql)
@@ -524,8 +525,9 @@ def get_cousub_stats():
     sql = "SELECT s.ct_fips, s.ct_name, s.cs_fips, s.cs_name, s.sq_mi, s.pop, s.pop_sm, " \
         "CASE WHEN s.pop > 0 THEN s.pop_male / s.pop ELSE 0 END AS pct_male, " \
         "CASE WHEN s.pop > 0 THEN s.pop_female / s.pop ELSE 0 END AS pct_female, " \
-	"op.deaths AS opioid_deaths " \
+	"op.deaths AS opioid_deaths, pred.pred_diabetes as pct_diabetes " \
       "FROM synth_ma.cousub_stats s " \
+	"JOIN synth_ma.ma_cousub_pred pred ON pred.state_fips = '25' AND pred.county = s.ct_fips AND pred.county_sub_fips = s.cs_fips AND s.cs_fips != '00000' " \
 	"JOIN synth_ma.ma_opioid2 op ON op.cousubfp = s.cs_fips AND s.cs_fips != '00000' AND year = '2015' " \
       "WHERE s.cs_fips != '00000'"
     data = getData(con, sql)
